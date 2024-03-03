fc4b8555ba2227be6f1419aec7877c25b9f34b93
Victor <golovenko.vitya.05@mail.ru>
2024-03-03 23:07:05 +0700

    Старт перед БД + И

--- blocks/quadratic_equation/block_quadratic_equation.php
+++ blocks/quadratic_equation/block_quadratic_equation.php
@@ -36,43 +36,36 @@ class block_quadratic_equation extends block_base {
 
         $this->content = new stdClass;
 
-        //$this->content->text = '';
         $this->content->text .= '<form method="post">';
         $this->content->text .= '<input type="number" step=any name="a" placeholder="Значение a" required>';
         $this->content->text .= '<input type="number" step=any name="b" placeholder="Значение b" required>';
         $this->content->text .= '<input type="number" step=any name="c" placeholder="Значение c" required>';
         $this->content->text .= '<input type="submit" value="Решить" >';
         $this->content->text .= '</form>';
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $a = (float)$_POST['a'];
-            //optional_param()
-            $b = (float)$_POST['b'];
-            $c = (float)$_POST['c'];
 
-            if ($a == 0) {
-                $this->content->text .= 'Значение a не может быть равно 0';
-            }
-            else {
-                $discriminant = $b * $b - 4 * $a * $c;
+        $a = optional_param('a', 0, PARAM_FLOAT);
+        $b = optional_param('b', 0, PARAM_FLOAT);
+        $c = optional_param('c', 0, PARAM_FLOAT);
 
-                if ($discriminant > 0) {
-                    $x1 = (-$b + sqrt($discriminant)) / (2 * $a);
-                    $x2 = (-$b - sqrt($discriminant)) / (2 * $a);
-                    $this->content->text .= 'x1 = ' . $x1 . '<br>';
-                    $this->content->text .= 'x2 = ' . $x2;
-                } elseif ($discriminant == 0) {
-                    $x1 = $x2 = (-$b + sqrt($discriminant)) / (2 * $a);
-                    $this->content->text .= "Оба корня равны $x1";
-                } else {
-                    $this->content->text .= 'Нет корней!';
-                }
-            }
-// Вывод сообщения об ошибке, если введены нули
-//            else {
-//                $this->content->text .= 'Без нулей!';
-//            }
+        if ($a == 0) {
+            $this->content->text .= 'Значение a не может быть равно 0';
         }
+        else {
+            $discriminant = $b * $b - 4 * $a * $c;
 
+            if ($discriminant > 0) {
+                $x1 = (-$b + sqrt($discriminant)) / (2 * $a);
+                $x2 = (-$b - sqrt($discriminant)) / (2 * $a);
+                $this->content->text .= 'x1 = ' . $x1 . '<br>';
+                $this->content->text .= 'x2 = ' . $x2;
+            } elseif ($discriminant == 0) {
+                $x1 = $x2 = (-$b + sqrt($discriminant)) / (2 * $a);
+                $this->content->text .= "Оба корня равны $x1";
+            } else {
+                $this->content->text .= 'Нет корней!';
+            }
+        }
+        
         return $this->content;
     }
 
@@ -82,15 +75,3 @@ class block_quadratic_equation extends block_base {
     }
 
 }
-
-////если переменная 'а' передана, то...
-//if (isset($_POST["a"])) {
-//    //Вставляем данные, подставляя их в запрос
-//    $sql = mysqli_query($link, "INSERT INTO `quadratic_equation_history` (`a`, `b`, `c`, `x1`, `x2`) VALUES ('{$_POST['a']}', '{$_POST['b']}', '{$_POST['c']}', '{$_POST['x1']}', '{$_POST['x2']}')");
-//    //Если вставка прошла успешно
-//    if ($sql) {
-//        echo '<p>Данные успешно добавлены в таблицу.</p>';
-//    } else {
-//        echo '<p>Произошла ошибка: ' . mysqli_error($link) . '</p>';
-//    }
-//}
